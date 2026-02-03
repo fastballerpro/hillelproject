62e48b283cbc402781efaf7763c0aaae3e1aa50d
John Appleseed <john@gmail.com>
2026-02-03 20:18:42 +0200

    database settings

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -1,8 +1,22 @@
 from pydantic_settings import BaseSettings
 
 
+class DatabaseSettings(BaseSettings):
+    DEBUG: bool = False
+    PGHOST: str
+    PGDATABASE: str
+    PGUSER: str
+    PGPASSWORD: str
+    PGPORT: int = 5432
+
+    @property
+    def database_url(self) -> str:
+        return f'postgresql+asyncpg://{self.PGUSER}:{self.PGPASSWORD}@{self.PGHOST}:{self.PGPORT}/{self.PGDATABASE}?sslmode=require&channel_binding=require'
+
+
 class Settings(BaseSettings):
     DEBUG: bool = False
+
 
 
 settings = Settings()
