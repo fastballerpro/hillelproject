9ab25c8708d522eca3e0c8041f652ff7675f3354
fastcoderingman <243401656+fastballerpro@users.noreply.github.com>
2026-02-10 20:05:35 +0200

    create user method (#13)
    * create engine
    
    * create user method
    
    ---------
    
    Co-authored-by: John Appleseed <john@gmail.com>

---
+++ backend/app/apps/users/crud.py
@@ -0,0 +1,19 @@
+from backend.app.apps.auth.password_handler import PasswordHandler
+from backend.app.apps.users.models import User
+from backend.app.apps.users.schemas import RegisterUserSchema, UserBaseFieldsSchemas
+
+
+class UserManager:
+    async def create_user(self, session, user_register_data: RegisterUserSchema):
+        hashed_password = await PasswordHandler.get_password_hash(user_register_data.password)
+        user = User(
+            name=user_register_data.name,
+            email=user_register_data.email,
+            hashed_password=hashed_password,
+        )
+        session.add(user)
+        await session.commit()
+        return user
+    
+
+user_manager = UserManager()
