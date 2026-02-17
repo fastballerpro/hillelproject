077deb962be4bd4b7b6f4d0464c69ac8c3a96824
John Appleseed <john@gmail.com>
2026-02-17 19:31:26 +0200

    add index endpoint

--- frontend/app/main.py
+++ frontend/app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 
+from routers import router
+
 def get_application() -> FastAPI:
     _app = FastAPI(
         debug=True
     )
+    _app.include_router(router)
     return _app
 
 app = get_application()
