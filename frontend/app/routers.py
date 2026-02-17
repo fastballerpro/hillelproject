b6cc3734d245f6c0e887ae6421ee9df518f04782
John Doe <john@email.com>
2026-02-17 20:35:51 +0100

    frontend html

--- frontend/app/routers.py
+++ frontend/app/routers.py
@@ -2,15 +2,15 @@ from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
-
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory='templates')
 
 
 @router.get("/")
 async def index(requests: Request):
     context = {
-        "request": requests
+        'request': requests
     }
-    response = templates.TemplateResponse("pages/index.html", context)
+    response = templates.TemplateResponse('pages/index.html', context=context)
+
 
     return response
