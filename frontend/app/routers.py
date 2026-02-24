49f7c257daff8f0b2288d9657877ad57f72ffd08
John Appleseed <john@gmail.com>
2026-02-24 21:12:20 +0200

    fix conflicts

--- frontend/app/routers.py
+++ frontend/app/routers.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Form, status
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import RedirectResponse
+import httpx
 
 router = APIRouter()
+
 templates = Jinja2Templates(directory='templates')
 
 
@@ -14,3 +17,37 @@ async def index(requests: Request):
 
 
     return response
+
+
+@router.get("/sign-up")
+@router.post("/sign-up")
+async def user_register(requests: Request, username: str = Form(""), email: str = Form(""), password: str = Form("")):
+    context = {
+        'request': requests
+    }
+    if requests.method == "GET":
+        response = templates.TemplateResponse('pages/sign-up.html', context=context)
+        return response
+
+    async with httpx.AsyncClient() as client:
+        headers = {
+            'accept': 'application/json',
+            'Content-Type': 'application/json',
+        }
+
+        json_data = {
+            'password': password,
+            'email': email,
+            'name': username,
+        }
+        response = await client.post('http://backend:8000/users/create', json=json_data, headers=headers)
+
+        if response.status_code == status.HTTP_201_CREATED:
+            redirect_response = RedirectResponse(requests.url_for("index"), status_code=status.HTTP_303_SEE_OTHER)
+            return redirect_response
+        elif response.status_code == status.HTTP_409_CONFLICT:
+            context['username'] = username
+            context['email'] = email
+            context['error'] = "Користувач з таким email вже існує"
+            response = templates.TemplateResponse('pages/sign-up.html', context=context)
+            return response
